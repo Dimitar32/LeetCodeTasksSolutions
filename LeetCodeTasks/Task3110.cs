2fb2f590407c14ad4f3c37dc87ee82b5ec33ba4e
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2025-03-10 22:58:59 +0200

    Add project files.

---
+++ LeetCodeTasks/Task3110.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTask3110.LeetCodeTasks
+{
+    internal class Task3110
+    {
+        public static int ScoreOfString(string s)
+        {
+            int sum = 0;
+
+            if (s.Length == 0)
+            {
+                return sum;
+            }
+
+            if (s.Length == 1)
+            {
+                sum += (char)s[0];
+                return sum;
+            }
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                char c1 = s[i];
+                char c2 = s[i + 1];
+                sum += Math.Abs(c1 - c2);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeTask3110.LeetCodeTasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTask3110
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Task3110.ScoreOfString("hello"));
+
+        }
+    }
+}
