30932d66d83e1b10b6d0b4bff331d9f0f4bf263e
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2025-03-24 20:47:44 +0200

    add task 1512

---
+++ LeetCodeTasks/Task1512.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTask3110.LeetCodeTasks
+{
+    internal class Task1512
+    {
+
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            if (nums == null || nums.Length == 1)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 2)
+            {
+                return nums[0] == nums[1] ? 1 : 0;
+            }
+
+            List<int[]> result = new List<int[]>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] == nums[i])
+                    {
+                        result.Add(new int[] {i, j});
+                    }
+                }
+            }
+
+            return result.Count;
+
+        }
+
+    }
+}
