7457d40ff67eb1b54f7026b20dc391f5c3f9aeaa
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2025-04-23 09:56:23 +0300

    add task 0069

---
+++ LeetCodeTasks/Task0069.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTask3110.LeetCodeTasks
+{
+    internal class Task0069
+    {
+        public static int MySqrt(int x)
+        {
+            return (int)Math.Floor(Math.Sqrt(x));
+        }
+    }
+}
