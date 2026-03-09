486bfee1912c8ed8a84778694f2b52e611fad0e5
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2026-03-09 13:24:57 +0200

    add task 184 m

---
+++ LeetCodeTasks/Medium/Task0184.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+;with cte as(
+    select e.name, e.salary, d.name as Department, dense_rank() over(partition by e.departmentid order by e.salary desc) rn
+    from Employee e
+    inner join Department d on e.departmentid = d.id
+)
+select c.Department, c.name as Employee, c.salary as Salary
+from cte c 
+where c.rn = 1
