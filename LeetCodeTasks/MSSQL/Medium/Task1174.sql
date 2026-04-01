f6d9b654483dd85aaa4132b6d12e5173ea274330
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2026-04-01 09:33:43 +0300

    add task 1174m sql

---
+++ LeetCodeTasks/MSSQL/Medium/Task1174.sql
@@ -0,0 +1,19 @@
+﻿/* Write your T-SQL query statement below */
+;with cte as
+(
+    select
+            row_number() over(partition by customer_id order by order_date) as rn,
+            customer_id,
+            delivery_id,
+            iif(order_date = customer_pref_delivery_date, 1, 0) as [immediate]
+    from Delivery
+)
+select cast(
+            cast(sum([immediate]) as decimal(9,4))
+            /
+            cast(count([immediate]) as decimal(9,4))
+            as decimal(9,4)
+            ) * 100
+            as immediate_percentage 
+from cte
+where rn = 1
