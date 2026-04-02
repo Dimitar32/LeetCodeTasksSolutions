7a1af6aacfa1eadc6d8ef16c304956cd6bba0a23
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2026-04-02 10:11:27 +0300

    add task 1141 sql easy

---
+++ LeetCodeTasks/MSSQL/Easy/Task1141.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+
+select distinct activity_date as [day], count(distinct user_id) as active_users
+from Activity
+where activity_date  between dateadd(day, -29, '2019-07-27') and '2019-07-27'
+group by activity_date 
+order by activity_date
